359d05db849e9c37ce438e1833e5450e92423199
jnguy707 <jeremy.n.707@live.com>
2024-05-19 04:15:58 -0700

    get:/list-urls developed

--- api/main.py
+++ api/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+from requests.URLRequest import URLRequest
+from models.URLModel import URLModel
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message:": "Hello World"}
+
+'''
+Considerations
+-Index Queries
+-Pagination
+'''
+@app.get("/list-urls")
+async def get_list_urls():
+    try:
+        return list(URLModel.scan())
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
